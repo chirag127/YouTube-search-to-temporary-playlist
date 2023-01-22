d046f1f2d74a77f66f21467aeb1e046eb358948e
Chirag <76880977+chirag127@users.noreply.github.com>
2023-01-22 13:00:22 +0000

    done

--- app.py
+++ app.py
@@ -5,19 +5,20 @@ INVIDIOUS_URL = "https://vid.puffyan.us"
 
 def search_videos(query, page=1):
     # Call the Invidious API to search for videos
-    url = f"{INVIDIOUS_URL}/api/v1/search?q={query}&page={page}"
+    url = f"{INVIDIOUS_URL}/api/v1/search?q={query}&page={page}&type=video"
     response = requests.get(url)
     data = response.json()
-
+    print(data)
     # Get video URLs from search results
-    video_urls = []
-    for item in data["results"]:
-        video_urls.append(f"{INVIDIOUS_URL}/watch?v={item['videoId']}")
-    return video_urls
+    video_ids = []
+    for item in data:
 
-def create_playlist(video_urls):
+        video_ids.append(f"{item['videoId']}")
+    return video_ids
+
+def create_playlist(video_ids):
     # Create a playlist using the video URLs
-    playlist_url = f"http://www.youtube.com/watch_videos?video_ids={','.join(video_urls)
+    playlist_url = f"http://www.youtube.com/watch_videos?video_ids={','.join(video_ids)}"
     return playlist_url
 
 def main():
@@ -28,11 +29,11 @@ def main():
 
     # Get video URLs and create playlist
     if st.button("Create Playlist"):
-        video_urls = []
+        video_ids = []
         for page in range(1, 5):
-            video_urls.extend(search_videos(search_term, page))
-        if video_urls:
-            playlist_url = create_playlist(video_urls)
+            video_ids.extend(search_videos(search_term, page))
+        if video_ids:
+            playlist_url = create_playlist(video_ids)
             st.success("Playlist created!")
             st.text("Playlist URL:")
             st.text(playlist_url)
